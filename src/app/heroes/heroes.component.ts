a8e2604664af399ecfcecc7f056008f08a65beaa
liguangyi <252438823@qq.com>
2019-08-05 23:20:31 +0800

    update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -22,14 +23,18 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
     this.getHeros();
   }
 
   getHeros(): void {
-    this.heroes = this.heroService.getHeroes()
+    this.messageService.add('HeroService: fetched heroes');
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
 }
